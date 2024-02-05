0d73adc5edc4c2cb9e62ae1ee634398d8dfd7b9c
Khang Ngo <khangngo86@gmail.com>
2024-02-05 09:54:21 -0500

    initial structure upload and etl.ipynb

---
+++ website/jupyter/_config.py
@@ -0,0 +1,3 @@
+# To obtain a key, go to https://www.eia.gov/opendata/
+# After updating the key, save the file name as config.py (without the underscore in the front)
+api_key = "<Enter you key here>"
